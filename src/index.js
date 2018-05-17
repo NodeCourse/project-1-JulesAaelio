7300b80ce57d8bd26a5bea66b665be28ffadfcf2
Jules LAURENT <jules.laurent@ynov.com>
2018-05-17 14:11:23 +0200

    error on signup with used address

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.get('/login',(req,res) =>{
 app.post('/login',
     passport.authenticate('local', {
         successRedirect: '/',
-        failureRedirect: '/login'
+        failureRedirect: '/login',
     })
 );
 
