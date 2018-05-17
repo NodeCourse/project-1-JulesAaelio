50822728ff8fe893ca4ea171ed09bdecc9405b38
Jules LAURENT <jules.laurent@ynov.com>
2018-05-17 23:57:37 +0200

    percent display

--- src/endpoints/survey-usage.js
+++ src/endpoints/survey-usage.js
@@ -4,10 +4,26 @@ module.exports = (app, db) => {
             if (req.user) {
                 db.Survey.findById(req.params.id, {
                     include: [
-                        {all: true}
+                        {
+                            model : db.Answer,
+                            include : [
+                                db.Vote
+                            ]
+                        }
                     ]
                 }).then(survey => {
                     if (survey) {
+                         // res.send(survey);
+                        let totalAnswer = 0;
+                        survey.answers.forEach((o) => {
+                           totalAnswer += o.votes.length;
+                        });
+                        if(totalAnswer === 0) totalAnswer = 1;
+                        survey.answers.forEach((o) => {
+                            o.ratio = (o.votes.length * 100)/totalAnswer;
+                            console.log(o.votes.length);
+                            console.log(totalAnswer);
+                        });
                         res.render('survey', {
                             survey
                         })
